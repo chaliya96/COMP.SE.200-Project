7e76c11a707d39982bf5f4c81e881a5b110db718
MasudHaider <masud6563@gmail.com>
2023-12-08 22:44:02 +0200

    more cases for memoize.js

--- src/tests/memoize.test.js
+++ src/tests/memoize.test.js
@@ -43,4 +43,37 @@ describe('memoize function', () => {
     expect(memoizedAdd.cache.has(key)).toBe(true);
     memoize.Cache = Map; // Reset to default
   });
+
+  test('handles function with no arguments', () => {
+    const noArgFunc = () => 42;
+    const memoized = memoize(noArgFunc);
+    expect(memoized()).toBe(42);
+  });
+  
+  test('handles function returning undefined', () => {
+    const undefinedFunc = () => undefined;
+    const memoized = memoize(undefinedFunc);
+    expect(memoized()).toBeUndefined();
+  });
+  
+  test('handles function throwing error', () => {
+    const errorFunc = () => { throw new Error('Test error'); };
+    const memoized = memoize(errorFunc);
+    expect(memoized).toThrow('Test error');
+  });
+  
+  test('handles resolver throwing error', () => {
+    const simpleAdd = (a, b) => a + b;
+    const errorResolver = () => { throw new Error('Resolver error'); };
+    const memoized = memoize(simpleAdd, errorResolver);
+    expect(() => memoized(1, 2)).toThrow('Resolver error');
+  });
+  
+  // test('handles resolver returning non-string', () => {
+  //   const simpleAdd = (a, b) => a + b;
+  //   const nonStringResolver = (...args) => args;
+  //   const memoized = memoize(simpleAdd, nonStringResolver);
+  //   memoized(1, 2);
+  //   expect(memoized.cache.has('1,2')).toBe(true);
+  // });
 });
